9aa3846f27cf38dd579ad88299cc02f3e83cf411
Alect Prasad <alectprasad@gmail.com>
2024-11-20 22:44:15 -0500

    basic crud

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");   
+const methodOverride = require("method-override");
 const path = require("path");
 const CampGround = require("./models/campground");
 
@@ -15,14 +16,48 @@ db.once("open", () => {
 app.set("view engine", "ejs");
 app.set("views"), path.join(__dirname, "views");
 
+app.use(express.urlencoded({ extended:true }));
+app.use(methodOverride('_method'));
+
 app.get("/", (req, res) => {
     res.render("home");
 });
 
-app.get("/make", async (req, res) => {
-    const camp = new CampGround({ title: "test" });
-    await camp.save();
-    res.send(camp);
+app.get("/campgrounds", async (req, res) => {
+    const campgrounds = await CampGround.find({});
+    res.render("campgrounds/index", {campgrounds});
+});
+
+app.post("/campgrounds", async (req, res) => {
+    const campground = new CampGround(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+});
+
+app.get("/campgrounds/new", (req, res) => {
+    res.render("campgrounds/new");
+});
+
+app.get("/campgrounds/:id", async (req, res) => {
+    const campground = await CampGround.findById(req.params.id);
+    res.render("campgrounds/show", {campground});
+});
+
+app.get("/campgrounds/:id/edit", async (req, res) => {
+    const campground = await CampGround.findById(req.params.id);
+    res.render("campgrounds/edit", {campground});
+});
+
+app.put("/campgrounds/:id", async (req, res) => {
+    const { id } = req.params;
+    await CampGround.findByIdAndUpdate(id, {...req.body.campground});
+    res.redirect(`/campgrounds/${id}`);
+});
+
+app.delete("/campgrounds/:id", async (req, res) => {
+    const { id } = req.params;
+    await CampGround.findByIdAndDelete(id);
+    res.redirect("/campgrounds");
 });
 
 app.listen(3000, () => {
