acba1a5493cc50f9488edf67dfe17e828de04e21
Alect Prasad <alectprasad@gmail.com>
2024-11-22 23:31:15 -0500

    initial styling

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");   
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 const path = require("path");
 const CampGround = require("./models/campground");
 
@@ -15,6 +16,7 @@ db.once("open", () => {
 
 app.set("view engine", "ejs");
 app.set("views"), path.join(__dirname, "views");
+app.engine("ejs", ejsMate);
 
 app.use(express.urlencoded({ extended:true }));
 app.use(methodOverride('_method'));
--- seeds/index.js
+++ seeds/index.js
@@ -19,9 +19,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: `https://picsum.photos/400/${Math.floor(Math.random() * 1000)}`,
+            description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus nihil ad iusto doloremque quod possimus sed error quidem sapiente quos atque natus eveniet illum sit commodi ipsa, quas, quae aut.`,
+            price
         })
         await camp.save();
     }
